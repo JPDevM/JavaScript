65c0a7489cd885bf369ead2817d65dca66d73be4
Juan Pablo <devecchi@exxo.es>
2021-03-09 12:52:50 +0100

    add form html

---
+++ practice02/js/validateForm.js
@@ -0,0 +1,14 @@
+// UX
+// https://medium.com/nextux/forms-need-validation-2ecbccbacea1
+
+// General.
+// Icono de validación adentro a la derecha.
+// Los campos no pueden estár vaciós.
+// Los campos no pueden ser incorrectas.
+//
+// Cuando todos los inputs están validados se agrega la clase was-validated al form y este habilita el submit.
+
+// Input Name
+// Más de 5 carácteres.
+// Solo letras y espacios.
+//
